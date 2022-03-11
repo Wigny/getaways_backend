36e173c9519c0495af4462ceccc9afa66524fee5
Wígny Almeida <wignybora@gmail.com>
2022-03-11 03:41:31 +0000

    feat: add place query

--- lib/getaways_web/schema.ex
+++ lib/getaways_web/schema.ex
@@ -1,3 +1,33 @@
 defmodule GetawaysWeb.Schema do
   use Absinthe.Schema
+
+  alias Getaways.Vacation
+
+  import_types Absinthe.Type.Custom
+
+  query do
+    @desc "Get a place by it's slug"
+    field :place, :place do
+      arg :slug, non_null(:string)
+
+      resolve fn _, %{slug: slug}, _ ->
+        {:ok, Vacation.get_place_by_slug!(slug)}
+      end
+    end
+  end
+
+  object :place do
+    field :id, non_null(:id)
+    field :name, non_null(:string)
+    field :location, non_null(:string)
+    field :slug, non_null(:string)
+    field :description, non_null(:string)
+    field :max_guests, non_null(:integer)
+    field :pet_friendly, non_null(:boolean)
+    field :pool, non_null(:boolean)
+    field :wifi, non_null(:boolean)
+    field :price_per_night, non_null(:decimal)
+    field :image, non_null(:string)
+    field :image_thumbnail, non_null(:string)
+  end
 end
